25147fbbbc02360a65d3aa2937afbb352a4f212e
Shubham Prakash <prakash.shubhamjha@gmail.com>
2022-06-21 18:09:46 +0530

    FIX: cors issue

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const productRoutes = require('./routes/products');
 const cartRoutes = require('./routes/cart');
 
@@ -9,6 +10,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
