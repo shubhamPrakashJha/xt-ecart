3b8a6b29807decbb83aeb212118c9838e7241c5c
Shubham Prakash <prakash.shubhamjha@gmail.com>
2022-06-22 20:14:53 +0530

    product-api: Add Pagination meta data

--- server/routes/products.js
+++ server/routes/products.js
@@ -6,13 +6,23 @@ const { products } = require('../mock/products');
 
 // GET all products
 router.get('/', (req, res) => {
+  let response = {};
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 100;
-
   const startIndex = (page - 1) * limit;
 
   const productsSliced = products.slice(startIndex, startIndex + limit);
-  res.json(productsSliced);
+
+  response = {
+    _metadata: {
+      page: page,
+      limit: limit,
+      pageCount: products.length / limit,
+      totalCount: products.length,
+    },
+    records: productsSliced,
+  };
+  res.json(response);
 });
 
 // GET a single product
